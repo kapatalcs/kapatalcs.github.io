df3d8a820f0d727e4eb9ca5e500974e51f9d3189
Yusuf İslam ÖZAYDIN <67331193+kapatalcs@users.noreply.github.com>
2025-06-27 22:01:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,30 @@ closeIcon.addEventListener("click", function(){
     sideBar.classList.add("close-sidebar");
     
 })
+
+
+const form = document.getElementById('contactForm');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault(); // Sayfa yenilenmesini engelle
+
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        window.location.href = 'https://kapatalcs.github.io';
+      } else {
+        alert('Mesaj gönderilemedi. Lütfen tekrar deneyin.');
+      }
+    })
+    .catch(error => {
+      alert('Bir hata oluştu: ' + error.message);
+    });
+  });
